e85c53f5d54428288d8c0039e35849cfce25ba81
Keyur <keyurborad5@gmail.com>
2022-03-14 12:49:52 +0530

    Rs Aligned depth if RS API file with color and depth streams aligned. Dematic visit code is code which was created to showcase at the Dematic visit it is again image processing based approach code

---
+++ realsense_aligned_depth.py
@@ -0,0 +1,51 @@
+import pyrealsense2 as rs
+import numpy as np
+
+
+
+class DepthCamera:
+    def __init__(self):
+        # Configure depth and color streams
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+
+        # Get device product line for setting a supporting resolution
+        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        pipeline_profile = config.resolve(pipeline_wrapper)
+        device = pipeline_profile.get_device()
+        device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+        config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 30)
+
+
+        # Start streaming
+        self.pipeline.start(config)
+
+    def get_frame(self):
+        # Create an align object
+        # rs.align allows us to perform alignment of depth frames to others frames
+        # The "align_to" is the stream type to which we plan to align depth frames.
+        align_to = rs.stream.color
+        align = rs.align(align_to)
+
+        frames = self.pipeline.wait_for_frames()
+
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
+        # Get aligned frames
+        depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        #Use below frames when you are not aligning depth and color stream, else keep it commented
+        #depth_frame = frames.get_depth_frame()
+        #color_frame = frames.get_color_frame()
+
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        if not depth_frame or not color_frame:
+            return False, None, None
+        return True, depth_image, color_image
+
+    def release(self):
+        self.pipeline.stop()
